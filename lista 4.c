92fb34e26ca95ab924e2c1bcb6928fca243e9a13
Lucas Frenhan <95430926+LucasFrenhan@users.noreply.github.com>
2022-09-17 17:47:46 -0300

    Add files via upload

--- lista 4.c
+++ lista 4.c
@@ -1,4 +1,4 @@
-#define ex3
+#define ex4
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -14,20 +14,6 @@ OBS.: Todos os programa devem ser finalizados pelo usuario.
 
 
 
-
-
-4 - Escreva um programa para receber via teclado em vetores 3 int, 3 long,
-    3 unsigned, 3 float e 3 double e imprima-os no video no seguinte formato:
-
-            10        20        30        40        50
-    12345678901234567890123456789012345678901234567890
-      int                 long                unsigned
-                float               double
-      int                 long                unsigned
-                float               double
-      int                 long                unsigned
-                float               double
-
 5 - Receba via teclado 10 valores inteiros e ordene por ordem crescente assim
     que sao digitados. Guarde-os em um vetor. Mostre ao final os valores ordenados.
 */
@@ -38,70 +24,94 @@ OBS.: Todos os programa devem ser finalizados pelo usuario.
    as componentes de ordem par. Por exemplo, se o vetor dado for v = {3, 5, 6, 8, 1, 4, 2, 3, 7},
    o programa deve gerar os vetores u = {3, 6, 1, 2, 7} e w = {5, 8, 4, 3}. */
 
-int v[] = {3, 5, 6, 8, 1, 4, 2, 3, 7};
+int vetorA[10];
+
+void criaVetor()
+{
+    int c;
+
+    for(c=0; c<10; c++)
+    {
+        printf("Digite o %d°:",c+1);
+        scanf("%d",&vetorA[c]);
+    }
+}
+
+void converteVetorPar()
+{
+    int vetorPar[5],p=0,c;
+
+    for(c=0; c<10; c++)
+    {
+        if(c%2==0)
+        {
+            vetorPar[p]=vetorA[c];
+            p++;
+        }
+    }
+
+
+    printf("\nVetor dos indices pares[");
+    for(p=0; p<5; p++)
+    {
+        if(p<4)
+        {
+            printf("%d, ",vetorPar[p]);
+        }
+        else
+        {
+            printf("%d",vetorPar[p]);
+        }
+    }
+    printf("]\n");
+
+}
+
+void converteVetorImpar()
+{
+    int vetorImpar[5],i=0,c;
+
+    for(c=0; c<10; c++)
+    {
+        if(c%2!=0)
+        {
+            vetorImpar[i]=vetorA[c];
+            i++;
+        }
+    }
+
+
+    printf("\nVetor dos indices impares[");
+    for(i=0; i<5; i++)
+    {
+        if(i<4)
+        {
+            printf("%d, ",vetorImpar[i]);
+        }
+        else
+        {
+            printf("%d",vetorImpar[i]);
+        }
+    }
+    printf("]\n");
+}
 
 int main()
 {
     setlocale (LC_ALL,"portuguese");
-    int i,vetorPar[5],vetorImpar[4],j=0,k=0;
+    int result;
 
-    for (i=0;i<9;i++)
+    do
     {
-        if(i%2==0)
-        {
-            vetorPar[j]=v[i];
-            j++;
-        }
-        else
-        {
-            vetorImpar[k]=v[i];
-            k++;
-        }
+        system("cls");
+        criaVetor();
+        converteVetorPar();
+        converteVetorImpar();
+        printf("\nDigite 1 se quiser executar novamente.");
+        printf("\nPara encerrar o programa digite qualquer número.\n");
+        scanf("%d", &result);
     }
-
-    printf("\nVetor dos indices que será convertido [");
-    for (i=0;i<10;i++)
-    {
-        if(i<9)
-        {
-            printf("%d, ",v[i]);
-        }
-        else
-        {
-            printf("%d",v[i]);
-        }
-
-    }
-    printf("]\n");
-
-    printf("\nVetor dos indices pares[");
-    for(j=0;j<5;j++)
-    {
-        if(j<4)
-        {
-            printf("%d, ",vetorPar[j]);
-        }
-        else
-        {
-            printf("%d",vetorPar[j]);
-        }
-
-    }
-    printf("]\n");
-
-     printf("\nVetor dos indices impares[");
-     for(k=0;k<5;k++)
-    {
-        if(k<4)
-        {
-            printf("%d, ",vetorImpar[k]);
-        }
-        else
-        {
-            printf("%d",vetorImpar[k]);
-        }
-    }
-    printf("]\n");
+    while (result == 1);
     return 0;
 }
 #endif // ex1
@@ -169,19 +179,21 @@ int main()
 
     do
     {
-    system("cls");
-    printf("Digite um valor: ");
-    scanf("%d",&numero);
-    criaVetor();
-    kx = verificaVetor(numero);
-    if(kx==0)
-    {
-        printf("O número digitado não está no vetor\n");
-    }
-    imprimiVetor();
-    printf("\nDigite 1 se quiser executar novamente.");
-    printf("\nPara encerrar o programa digite qualquer número.\n");
-    scanf("%d", &result);
+        system("cls");
+        printf("Digite um valor: ");
+        scanf("%d",&numero);
+        criaVetor();
+        kx = verificaVetor(numero);
+
+        if(kx==0)
+        {
+            printf("O número digitado não está no vetor\n");
+        }
+
+        imprimiVetor();
+        printf("\nDigite 1 se quiser executar novamente.");
+        printf("\nPara encerrar o programa digite qualquer número.\n");
+        scanf("%d", &result);
     }
     while (result == 1);
 }
@@ -295,16 +307,16 @@ int main()
 
     do
     {
-    criaVetorA();
-    criaVetorB();
-    criaVetorC();
-    imprimiVetorA();
-    imprimiVetorB();
-    imprimiVetorC();
+        criaVetorA();
+        criaVetorB();
+        criaVetorC();
+        imprimiVetorA();
+        imprimiVetorB();
+        imprimiVetorC();
 
-    printf("\nDigite 1 se quiser executar novamente.");
-    printf("\nPara encerrar o programa digite qualquer número.\n");
-    scanf("%d", &result);
+        printf("\nDigite 1 se quiser executar novamente.");
+        printf("\nPara encerrar o programa digite qualquer número.\n");
+        scanf("%d", &result);
     }
     while (result == 1);
 }
@@ -312,6 +324,131 @@ int main()
 #endif // ex3
 
 #ifdef ex4
+/*
+4 - Escreva um programa para receber via teclado em vetores 3 int, 3 long,
+    3 unsigned, 3 float e 3 double e imprima-os no video no seguinte formato:
+
+            10        20        30        40        50
+    12345678901234567890123456789012345678901234567890
+      int                 long                unsigned
+                float               double
+      int                 long                unsigned
+                float               double
+      int                 long                unsigned
+                float               double
+*/
+
+int vetorInt[3][1];
+float vetorFloat[3][1];
+long vetorLong[3][1];
+double vetorDouble[3][1];
+unsigned vetorUnsigned[3][1];
+
+int criaVetorInt()
+{
+    int i;
+
+    printf("Primeiro serão valores do tipo inteiros\n");
+    for(i=0; i<3; i++)
+    {
+        printf("\nDigite o %d° número: ",i+1);
+        scanf("%d",&vetorInt[i][1]);
+    }
+}
+
+float criaVetorFloat()
+{
+    int i;
+
+    printf("Segundo serão valores do tipo ponto flutuante\n");
+    for(i=0; i<3; i++)
+    {
+        printf("\nDigite o %d° número: ",i+1);
+        scanf("%f",&vetorFloat[i][1]);
+    }
+}
+
+long criaVetorLong()
+{
+    int i;
+
+    printf("Terceiro serão valores do tipo long\n");
+    for(i=0; i<3; i++)
+    {
+        printf("\nDigite o %d° número: ",i+1);
+        scanf("%ld",&vetorLong[i][1]);
+    }
+}
+
+double criaVetorDouble()
+{
+    int i;
+
+    printf("Quarto serão valores do tipo double\n");
+    for(i=0; i<3; i++)
+    {
+        printf("\nDigite o %d° número: ",i+1);
+        scanf("%lf",&vetorDouble[i][1]);
+    }
+}
+
+unsigned criaVetorUnsigned()
+{
+    int i;
+
+    printf("Quinto serão valores do tipo unsingned\n");
+    for(i=0; i<3; i++)
+    {
+        printf("\nDigite o %d° número: ",i+1);
+        scanf("%u",&vetorUnsigned[i][1]);
+    }
+}
+
+void imprimeVetores()
+{
+    int i,j=0,k=0;
+
+    printf("        10        20        30        40        50\n");
+    printf("12345678901234567890123456789012345678901234567890\n");
+
+    for(i=0; i<6; i++)
+    {
+        if(i%2==0)
+        {
+            printf("%5d%21d%24u\n",vetorInt[j][1],vetorLong[j][1],vetorUnsigned[j][1]);
+            j++;
+        }
+        else
+        {
+            printf("%17.1f%21.1lf\n",vetorFloat[k][1],vetorDouble[k][1]);
+            k++;
+        }
+    }
+}
 
 
+int main()
+{
+    setlocale (LC_ALL,"portuguese");
+    int result;
+
+    do
+    {
+        system("cls");
+
+        printf("Nosso programa recebera vários valores\n");
+        criaVetorInt();
+        criaVetorFloat();
+        criaVetorLong();
+        criaVetorDouble();
+        criaVetorUnsigned();
+        imprimeVetores();
+
+        printf("\nDigite 1 se quiser executar novamente.");
+        printf("\nPara encerrar o programa digite qualquer número.\n");
+        scanf("%d", &result);
+    }
+    while (result == 1);
+    return 0;
+}
 #endif // ex4
