ef306e7d65273ee6acbba9eb83e78b540afda489
Lucas Frenhan <95430926+LucasFrenhan@users.noreply.github.com>
2022-10-05 21:44:03 -0300

    desafio finalizado uhull!!

--- desafio.c
+++ desafio.c
@@ -15,7 +15,6 @@ void entradaDados()
     {
         for (i = 0; i < 3; i++)
         {
-            getchar();
             printf("\nDigite um filme:");
             gets(filme[i]);
             printf("\nDigite a produtora:");
@@ -34,7 +33,7 @@ void entradaDados()
 
 void listaDados()
 {
-    int i;
+    int i, controle = 0;
     FILE *arquivo;
 
     arquivo = fopen("filme.txt", "r");
@@ -44,10 +43,19 @@ void listaDados()
         for (i = 0; i < 3; i++)
         {
             fread(filme[i], sizeof(filme[i]), 1, arquivo);
-            printf("\nO filme:%s", filme[i]);
             fread(produtora[i], sizeof(produtora[i]), 1, arquivo);
-            printf("\nDa produtora:%s", produtora[i]);
+
+            if(filme[i][0] != '#')
+            {
+                printf("\nO filme:%s", filme[i]);
+                printf("\nDa produtora:%s", produtora[i]);
+                controle++;
+            }
         }
+
+        if(controle == 0)
+            printf("Nenhum filme cadastrado\n");
+
         fclose(arquivo);
     }
 
@@ -55,12 +63,11 @@ void listaDados()
     {
         printf("Arquivo não encontrado\n");
     }
-    fclose(arquivo);
 }
 
 void pesquisaFilme()
 {
-    int i, c, contador = 0, controle = 0;
+    int i, c, controle = 0;
     FILE *arquivo;
     char nome[30];
 
@@ -84,10 +91,11 @@ void pesquisaFilme()
                 break;
             }
 
-            for (c = 0; filme[i][c] != '\0'; c++)
+            for (c = 0; nome[c] != '\0'; c++)
             {
-                if (filme[i][c] != nome[c])
+                if ( nome[c] != filme[i][c] )
                 {
+                    controle = 0;
                     break;
                 }
                 else
@@ -96,23 +104,22 @@ void pesquisaFilme()
                 }
             }
         }
-
+        i--;
         if (controle == 0)
         {
             printf("\nNenhum filme foi encontrado\n");
         }
         else
         {
-            printf("\nO filme:%s", filme[i - 1]);
-            printf("\nDa produtora:%s", produtora[i - 1]);
-            printf("\n");
+            printf("\nO filme:%s", filme[i]);
+            printf("\nDa produtora:%s\n", produtora[i]);
         }
+        fclose(arquivo);
     }
     else
     {
         printf("Arquivo não encontrado\n");
     }
-    fclose(arquivo);
 }
 
 void pesquisaProdutora()
@@ -147,11 +154,160 @@ void pesquisaProdutora()
         {
             printf("\nNenhuma produtora foi encontrada");
         }
+        fclose(arquivo);
     }
     else
     {
         printf("\nArquivo não encontrado");
     }
+}
+
+void alteraDados()
+{
+    int i, c, j, controle = 0;
+    FILE *arquivo;
+    char nome[30];
+
+    arquivo = fopen("filme.txt", "r");
+
+    if (arquivo != NULL)
+    {
+        for (i = 0; i < 3; i++)
+        {
+            fread(filme[i], sizeof(filme[i]), 1, arquivo);
+            fread(produtora[i], sizeof(produtora[i]), 1, arquivo);
+        }
+
+        printf("\nDigite o nome do filme que deseja alterar:");
+        gets(nome);
+        for(i=0; i < 3; i++)
+        {
+            if(controle != 0)
+            {
+                break;
+            }
+
+            for (c = 0; filme[i][c] != '\0'; c++)
+            {
+                if ( nome[c] != filme[i][c] )
+                {
+                    controle = 0;
+                    break;
+                }
+                else
+                {
+                    controle++;
+                }
+            }
+        }
+
+        i--;
+        fclose(arquivo);
+
+        if (controle == 0)
+        {
+            printf("\nNenhum filme foi encontrado\n");
+        }
+        else
+        {
+            arquivo = fopen("filme.txt", "w");
+            printf("\nO filme:%s", filme[i]);
+            printf("\nO novo nome do filme:");
+            gets(nome);
+
+            for(j=0; nome[j]!='\0';j++)
+            {
+                filme[i][j] = nome[j];
+            }
+            filme[i][j] = '\0';
+            printf("\nO novo nome do filme:%s\n", filme[i]);
+
+            fwrite(filme[0], sizeof(filme[0]), 1, arquivo);
+            fwrite(produtora[0], sizeof(produtora[0]), 1, arquivo);
+            fwrite(filme[1], sizeof(filme[1]), 1, arquivo);
+            fwrite(produtora[1], sizeof(produtora[1]), 1, arquivo);
+            fwrite(filme[2], sizeof(filme[2]), 1, arquivo);
+            fwrite(produtora[2], sizeof(produtora[2]), 1, arquivo);
+        }
+        for(i = 0; i < 3; i++)
+        {
+            printf("%s\n",filme[i]);
+        }
+
+        fclose(arquivo);
+    }
+    else
+    {
+        printf("Arquivo não encontrado\n");
+    }
+}
+
+void excluiDados()
+{
+    int i, c, controle = 0;
+    FILE *arquivo;
+    char nome[30];
+
+    arquivo = fopen("filme.txt", "r");
+
+    if (arquivo != NULL)
+    {
+        for (i = 0; i < 3; i++)
+        {
+            fread(filme[i], sizeof(filme[i]), 1, arquivo);
+            fread(produtora[i], sizeof(produtora[i]), 1, arquivo);
+        }
+
+        printf("\nDigite o nome de um filme:");
+        gets(nome);
+
+        for(i=0; i < 3; i++)
+        {
+            if(controle != 0)
+            {
+                break;
+            }
+
+            for (c = 0; filme[i][c] != '\0'; c++)
+            {
+                if ( nome[c] != filme[i][c] )
+                {
+                    controle = 0;
+                    break;
+                }
+                else
+                {
+                    controle++;
+                }
+            }
+            fclose(arquivo);
+        }
+
+        i--;
+        if (controle == 0)
+        {
+            printf("\nNenhum filme foi encontrado\n");
+        }
+        else
+        {
+            arquivo = fopen("filme.txt", "w");
+            filme[i][0] = '#';
+
+            fwrite(filme[0], sizeof(filme[0]), 1, arquivo);
+            fwrite(produtora[0], sizeof(produtora[0]), 1, arquivo);
+            fwrite(filme[1], sizeof(filme[1]), 1, arquivo);
+            fwrite(produtora[1], sizeof(produtora[1]), 1, arquivo);
+            fwrite(filme[2], sizeof(filme[2]), 1, arquivo);
+            fwrite(produtora[2], sizeof(produtora[2]), 1, arquivo);
+        }
+
+    }
+    else
+    {
+        printf("Arquivo não encontrado\n");
+    }
+
+    fclose(arquivo);
 }
 
 void sair()
@@ -206,11 +362,18 @@ int main()
             break;
         }
         case 5:
-            printf("opc 5\n");
+        {
+            system("cls");
+            alteraDados();
             break;
+        }
+
         case 6:
-            printf("opc 6\n");
+        {
+            system("cls");
+            excluiDados();
             break;
+        }
         case 7:
             sair();
         default:
